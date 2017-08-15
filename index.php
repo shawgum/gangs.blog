b19765cd0e3d0e64615feb55c48920eeb1714c23
shawgum <shawgum@gmail.com>
2017-08-15 15:24:50 +0200

    use online jquery

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ include("resources/texts/research_on_influxdb.php");
 <head>
     <title>Gavin's Blog</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <!--    <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,600" rel="stylesheet">-->
+    <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,600" rel="stylesheet">
     <link rel="stylesheet" href="resources/stylesheets/typography.css" type="text/css"/>
     <link rel="stylesheet" href="resources/stylesheets/layout.css" type="text/css"/>
-    <script src="resources/scripts/jquery-3.2.1.min.js"></script>
-    <!--    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>-->
+    <!--    <script src="resources/scripts/jquery-3.2.1.min.js"></script>-->
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
     <script src="resources/scripts/global.js"></script>
 </head>
 <body>
